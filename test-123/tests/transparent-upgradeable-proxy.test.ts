a5e6435a060543de547a991a8c0ca5b1eb8bd03e
Tomás Migone <tomas@edgeandnode.com>
2024-07-29 17:46:24 -0300

    Initialize subgraph

--- test-123/tests/transparent-upgradeable-proxy.test.ts
+++ test-123/tests/transparent-upgradeable-proxy.test.ts
@@ -7,8 +7,8 @@ import {
   afterAll
 } from "matchstick-as/assembly/index"
 import { Address } from "@graphprotocol/graph-ts"
-import { AdminChanged } from "../generated/schema"
-import { AdminChanged as AdminChangedEvent } from "../generated/TransparentUpgradeableProxy/TransparentUpgradeableProxy"
+import { ExampleEntity } from "../generated/schema"
+import { AdminChanged } from "../generated/TransparentUpgradeableProxy/TransparentUpgradeableProxy"
 import { handleAdminChanged } from "../src/transparent-upgradeable-proxy"
 import { createAdminChangedEvent } from "./transparent-upgradeable-proxy-utils"
 
@@ -34,19 +34,19 @@ describe("Describe entity assertions", () => {
   // For more test scenarios, see:
   // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
 
-  test("AdminChanged created and stored", () => {
-    assert.entityCount("AdminChanged", 1)
+  test("ExampleEntity created and stored", () => {
+    assert.entityCount("ExampleEntity", 1)
 
     // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
     assert.fieldEquals(
-      "AdminChanged",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
       "previousAdmin",
       "0x0000000000000000000000000000000000000001"
     )
     assert.fieldEquals(
-      "AdminChanged",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
       "newAdmin",
       "0x0000000000000000000000000000000000000001"
     )
